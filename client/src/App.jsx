4446e67f3e76662c64acf00d1b4f99e5c8aaa6d6
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-10-12 21:01:43 -0400

    starto

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Reservation from './Reservation';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Reservation />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/assets/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2022 Reservation App</p>
+    </footer>
+  );
+};
+
+export default Footer;
